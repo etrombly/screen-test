bf0b1f03bdb7d8ad3577f0a10e7d58956c858186
Eric Trombly <etrombly@yahoo.com>
2020-03-02 07:40:27 -0600

    moved usb initi before screen init

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,29 @@
 #![no_std]
 #![no_main]
 
-// Busy A4
-// CS   A3
-// SCK  A5
-// MOSI A7
-// DC   A2
-// RST  A1
-//
-// pick a panicking behavior
-// extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
-
-use rtfm::app;
-use rtfm::export::wfi;
-
-use embedded_graphics::prelude::*;
-use embedded_graphics::{drawable::Pixel, prelude::UnsignedCoord, Drawing};
-use profont::{ProFont12Point, ProFont14Point, ProFont24Point, ProFont9Point};
-
-use embedded_hal::spi::MODE_0;
+extern crate panic_abort;
+use byteorder::{BigEndian, ByteOrder};
+use embedded_graphics::{
+    prelude::{UnsignedCoord, *},
+    Drawing,
+};
+use embedded_hal::{digital::v2::OutputPin, spi::MODE_0};
+use rtfm::{app, export::wfi};
 use ssd1675::{
     command::{BufCommand, Command, DataEntryMode, IncrementAxis},
     interface::{DisplayInterface, Interface},
 };
-use stm32f1::stm32f103::Interrupt;
 use stm32f1xx_hal::{
     delay::Delay,
-    gpio::{
-        gpioa::{PA1, PA2, PA3, PA4},
-        gpiob::{PB5, PB6, PB7, PB8},
-        Output, PushPull,
-    },
     prelude::*,
-    serial::{Rx, Serial, Tx},
     spi::Spi,
-    i2c::{I2c, Mode, blocking_i2c},
-    timer::{Event, Timer},
+    usb::{Peripheral, UsbBus, UsbBusType},
 };
-use htu21d::Htu21df;
-use bmpx85::Bmpx85;
+use usb_device::{bus, prelude::*};
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
 // Debug
-use cortex_m_semihosting::hio;
-use core::fmt::Write;
+//use cortex_m_semihosting::{debug, hprintln};
 
 const LUT: [u8; 70] = [
     0x80, 0x60, 0x40, 0x00, 0x00, 0x00, 0x00, //LUT0: BB:     VS 0 ~7
@@ -93,13 +71,13 @@ impl From<u8> for Color {
     }
 }
 
-struct Display<'a, SPI, CS, BUSY, DC, RESET>
+pub struct Display<'a, SPI, CS, BUSY, DC, RESET>
 where
     SPI: embedded_hal::blocking::spi::Write<u8>,
-    CS: embedded_hal::digital::OutputPin,
-    BUSY: embedded_hal::digital::InputPin,
-    DC: embedded_hal::digital::OutputPin,
-    RESET: embedded_hal::digital::OutputPin,
+    CS: embedded_hal::digital::v2::OutputPin,
+    BUSY: embedded_hal::digital::v2::InputPin,
+    DC: embedded_hal::digital::v2::OutputPin,
+    RESET: embedded_hal::digital::v2::OutputPin,
 {
     interface: Interface<SPI, CS, BUSY, DC, RESET>,
     buff: &'a mut [u8],
@@ -108,33 +86,37 @@ where
 impl<'a, SPI, CS, BUSY, DC, RESET> Display<'a, SPI, CS, BUSY, DC, RESET>
 where
     SPI: embedded_hal::blocking::spi::Write<u8>,
-    CS: embedded_hal::digital::OutputPin,
-    BUSY: embedded_hal::digital::InputPin,
-    DC: embedded_hal::digital::OutputPin,
-    RESET: embedded_hal::digital::OutputPin,
+    CS: embedded_hal::digital::v2::OutputPin,
+    BUSY: embedded_hal::digital::v2::InputPin,
+    DC: embedded_hal::digital::v2::OutputPin,
+    RESET: embedded_hal::digital::v2::OutputPin,
 {
-    fn init<D: embedded_hal::blocking::delay::DelayMs<u8>>(&mut self, delay: &mut D) {
+    fn init<D: embedded_hal::blocking::delay::DelayMs<u8>>(
+        &mut self,
+        delay: &mut D,
+    ) -> Result<(), <SPI as embedded_hal::blocking::spi::Write<u8>>::Error> {
         self.interface.reset(delay);
-        Command::SoftReset.execute(&mut self.interface);
+        Command::SoftReset.execute(&mut self.interface)?;
         self.interface.busy_wait();
-        Command::AnalogBlockControl(ANALOG_BLOCK_CONTROL_MAGIC).execute(&mut self.interface);
-        Command::DigitalBlockControl(DIGITAL_BLOCK_CONTROL_MAGIC).execute(&mut self.interface);
-        Command::DriverOutputControl(0xF9, 0x00).execute(&mut self.interface);
+        Command::AnalogBlockControl(ANALOG_BLOCK_CONTROL_MAGIC).execute(&mut self.interface)?;
+        Command::DigitalBlockControl(DIGITAL_BLOCK_CONTROL_MAGIC).execute(&mut self.interface)?;
+        Command::DriverOutputControl(0xF9, 0x00).execute(&mut self.interface)?;
         Command::DataEntryMode(
             DataEntryMode::IncrementYIncrementX,
             IncrementAxis::Horizontal,
         )
-        .execute(&mut self.interface);
-        Command::StartEndXPosition(0x0, 0x0F).execute(&mut self.interface); //0x0F-->(15+1)*8=128
-        Command::StartEndYPosition(0x0, 0xF9).execute(&mut self.interface); //0xF9-->(249+1)=250
-        Command::BorderWaveform(0x03).execute(&mut self.interface);
-        Command::WriteVCOM(0x55).execute(&mut self.interface);
-        Command::GateDrivingVoltage(0x15).execute(&mut self.interface);
-        Command::SourceDrivingVoltage(0x41, 0xA8, 0x32).execute(&mut self.interface);
-        Command::DummyLinePeriod(0x30).execute(&mut self.interface);
-        Command::GateLineWidth(0x0A).execute(&mut self.interface);
-        BufCommand::WriteLUT(&LUT).execute(&mut self.interface);
+        .execute(&mut self.interface)?;
+        Command::StartEndXPosition(0x0, 0x0F).execute(&mut self.interface)?; //0x0F-->(15+1)*8=128
+        Command::StartEndYPosition(0x0, 0xF9).execute(&mut self.interface)?; //0xF9-->(249+1)=250
+        Command::BorderWaveform(0x03).execute(&mut self.interface)?;
+        Command::WriteVCOM(0x55).execute(&mut self.interface)?;
+        Command::GateDrivingVoltage(0x15).execute(&mut self.interface)?;
+        Command::SourceDrivingVoltage(0x41, 0xA8, 0x32).execute(&mut self.interface)?;
+        Command::DummyLinePeriod(0x30).execute(&mut self.interface)?;
+        Command::GateLineWidth(0x0A).execute(&mut self.interface)?;
+        BufCommand::WriteLUT(&LUT).execute(&mut self.interface)?;
         self.interface.busy_wait();
+        Ok(())
     }
 
     fn set_pixel(&mut self, x: u32, y: u32, color: Color) {
@@ -151,69 +133,95 @@ where
         }
     }
 
-    fn update(&mut self) {
-        Command::XAddress(0x0).execute(&mut self.interface);
-        Command::YAddress(0x0).execute(&mut self.interface);
-        self.interface.send_command(0x24);
-        self.interface.send_data(self.buff);
-        Command::UpdateDisplayOption2(0xC7).execute(&mut self.interface);
-        Command::UpdateDisplay.execute(&mut self.interface);
+    fn update(&mut self) -> Result<(), <SPI as embedded_hal::blocking::spi::Write<u8>>::Error> {
+        Command::XAddress(0x0).execute(&mut self.interface)?;
+        Command::YAddress(0x0).execute(&mut self.interface)?;
+        self.interface.send_command(0x24)?;
+        self.interface.send_data(self.buff)?;
+        Command::UpdateDisplayOption2(0xC7).execute(&mut self.interface)?;
+        Command::UpdateDisplay.execute(&mut self.interface)?;
         self.interface.busy_wait();
+        Ok(())
     }
 }
 
-#[app(device = stm32f1::stm32f103)]
+pub type DISPLAY = Display<
+    'static,
+    stm32f1xx_hal::spi::Spi<
+        stm32f1::stm32f103::SPI1,
+        stm32f1xx_hal::spi::Spi1NoRemap,
+        (
+            stm32f1xx_hal::gpio::gpioa::PA5<
+                stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::PushPull>,
+            >,
+            stm32f1xx_hal::gpio::gpioa::PA6<
+                stm32f1xx_hal::gpio::Input<stm32f1xx_hal::gpio::Floating>,
+            >,
+            stm32f1xx_hal::gpio::gpioa::PA7<
+                stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::PushPull>,
+            >,
+        ),
+    >,
+    stm32f1xx_hal::gpio::gpioa::PA3<stm32f1xx_hal::gpio::Output<stm32f1xx_hal::gpio::PushPull>>,
+    stm32f1xx_hal::gpio::gpioa::PA4<stm32f1xx_hal::gpio::Input<stm32f1xx_hal::gpio::Floating>>,
+    stm32f1xx_hal::gpio::gpioa::PA2<stm32f1xx_hal::gpio::Output<stm32f1xx_hal::gpio::PushPull>>,
+    stm32f1xx_hal::gpio::gpioa::PA1<stm32f1xx_hal::gpio::Output<stm32f1xx_hal::gpio::PushPull>>,
+>;
+#[app(device = stm32f1::stm32f103, peripherals = true)]
 const APP: () = {
-    //static mut SLEEP: u32 = 0;
+    struct Resources {
+        usb_dev: UsbDevice<'static, UsbBusType>,
+        serial: SerialPort<'static, UsbBusType>,
+        display: DISPLAY,
+        #[init([255u8; ROWS as usize * COLS as usize / 8])]
+        buffer: [u8; ROWS as usize * COLS as usize / 8],
+        #[init(0)]
+        index: u32,
+        #[init(0)]
+        size: u32,
+    }
 
-    #[init]
-    fn init() {
-        let device: stm32f1::stm32f103::Peripherals = device;
-        let core: rtfm::Peripherals = core;
-        let mut flash = device.FLASH.constrain();
-        let mut rcc = device.RCC.constrain();
-        let mut afio = device.AFIO.constrain(&mut rcc.apb2);
+    #[init(resources = [buffer])]
+    fn init(cx: init::Context) -> init::LateResources {
+        let cp = cortex_m::Peripherals::take().unwrap();
+        let mut flash = cx.device.FLASH.constrain();
+        let mut rcc = cx.device.RCC.constrain();
+        let mut afio = cx.device.AFIO.constrain(&mut rcc.apb2);
+        static mut USB_BUS: Option<bus::UsbBusAllocator<UsbBusType>> = None;
 
         let clocks = rcc
             .cfgr
-            .sysclk(64.mhz())
-            .pclk1(32.mhz())
+            .use_hse(8.mhz())
+            .sysclk(48.mhz())
+            .pclk1(24.mhz())
             .freeze(&mut flash.acr);
 
-        let mut delay = Delay::new(core.SYST, clocks);
+        assert!(clocks.usbclk_valid());
 
-        let mut gpioa = device.GPIOA.split(&mut rcc.apb2);
-        let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
+        let mut delay = Delay::new(cp.SYST, clocks);
+
+        let mut gpioa = cx.device.GPIOA.split(&mut rcc.apb2);
+
+        // BluePill board has a pull-up resistor on the D+ line.
+        // Pull the D+ pin down to send a RESET condition to the USB bus.
+        // This forced reset is needed only for development, without it host
+        // will not reset your device when you upload new firmware.
+        let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+        usb_dp.set_low().unwrap();
+        cortex_m::asm::delay(clocks.sysclk().0 / 100);
+
+        let usb_dm = gpioa.pa11;
+        let usb_dp = usb_dp.into_floating_input(&mut gpioa.crh);
+        let usb = Peripheral {
+            usb: cx.device.USB,
+            pin_dm: usb_dm,
+            pin_dp: usb_dp,
+        };
 
         let reset = gpioa.pa1.into_push_pull_output(&mut gpioa.crl);
         let dc = gpioa.pa2.into_push_pull_output(&mut gpioa.crl);
         let cs = gpioa.pa3.into_push_pull_output(&mut gpioa.crl);
         let busy = gpioa.pa4;
-
-        // I2C1 for htu21d
-        let scl1 = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
-        let sda1 = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
-
-        let i2c_hum = I2c::i2c1(device.I2C1, (scl1, sda1), &mut afio.mapr, Mode::Standard{frequency: 100_000}, clocks, &mut rcc.apb1);
-        let i2c_hum = blocking_i2c(i2c_hum, clocks, 1000,10,1000,1000);
-
-        //let mut htu21df = Htu21df::new(i2c_hum).unwrap();
-        //let hum = htu21df.get_humidity();
-
-        let mut stdout = hio::hstdout().unwrap();
-        //write!(stdout, "{:?}", hum).unwrap();
-
-        // I2C2 for bmp180
-        let scl2 = gpiob.pb10.into_alternate_open_drain(&mut gpiob.crh);
-        let sda2 = gpiob.pb11.into_alternate_open_drain(&mut gpiob.crh);
-
-        let i2c_press = I2c::i2c2(device.I2C2, (scl2, sda2), Mode::Standard{frequency: 100_000}, clocks, &mut rcc.apb1);
-        let i2c_press = blocking_i2c(i2c_press, clocks, 1000,10,1000,1000);
-
-        let mut bmpx85 = Bmpx85::new(i2c_press);
-        let chip = bmpx85.get_chip_id();
-
-        write!(stdout, "{:?}", chip).unwrap();
 
         // SPI1 display
         let sck = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
@@ -221,7 +229,7 @@ const APP: () = {
         let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
 
         let spi = Spi::spi1(
-            device.SPI1,
+            cx.device.SPI1,
             (sck, miso, mosi),
             &mut afio.mapr,
             MODE_0,
@@ -230,38 +238,99 @@ const APP: () = {
             &mut rcc.apb2,
         );
 
-        let mut controller = ssd1675::Interface::new(spi, cs, busy, dc, reset);
-        let mut buffer = [255u8; ROWS as usize * COLS as usize / 8];
+        let controller = ssd1675::Interface::new(spi, cs, busy, dc, reset);
         let mut display = Display {
             interface: controller,
-            buff: &mut buffer,
+            buff: cx.resources.buffer,
         };
-        display.init(&mut delay);
-        display.draw(
-            ProFont24Point::render_str("It Works")
-                .with_stroke(Some(Color::Black))
-                .with_fill(Some(Color::White))
-                .translate(Coord::new(1, -4))
-                .into_iter(),
+
+        if let Ok(_) = display.init(&mut delay) {};
+
+        if let Ok(_) = display.update() {};
+
+        unsafe {
+            USB_BUS = Some(UsbBus::new(usb));
+
+            let serial = SerialPort::new(USB_BUS.as_ref().unwrap());
+
+            let usb_dev =
+                UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0x16c0, 0x27dd))
+                    .manufacturer("Fake company")
+                    .product("Serial port")
+                    .serial_number("TEST")
+                    .device_class(USB_CLASS_CDC)
+                    .build();
+
+            init::LateResources {
+                usb_dev,
+                serial,
+                display,
+            }
+        }
+    }
+
+    #[task(binds = USB_HP_CAN_TX, resources = [usb_dev, serial, display, index, size])]
+    fn usb_tx(mut cx: usb_tx::Context) {
+        usb_poll(
+            &mut cx.resources.usb_dev,
+            &mut cx.resources.serial,
+            &mut cx.resources.display,
+            &mut cx.resources.index,
+            &mut cx.resources.size,
         );
-        display.draw(
-            ProFont9Point::render_str("Yes it does")
-                .with_stroke(Some(Color::Black))
-                .with_fill(Some(Color::White))
-                .translate(Coord::new(1, 24))
-                .into_iter(),
+    }
+
+    #[task(binds = USB_LP_CAN_RX0, resources = [usb_dev, serial, display, index, size])]
+    fn usb_rx0(mut cx: usb_rx0::Context) {
+        usb_poll(
+            &mut cx.resources.usb_dev,
+            &mut cx.resources.serial,
+            &mut cx.resources.display,
+            &mut cx.resources.index,
+            &mut cx.resources.size,
         );
-        display.update();
     }
 
     #[idle]
-    fn idle() -> ! {
+    fn idle(_cx: idle::Context) -> ! {
         loop {
             // wait for an interrupt (sleep)
             wfi();
         }
     }
 };
+
+fn usb_poll<B: bus::UsbBus>(
+    usb_dev: &mut UsbDevice<'static, B>,
+    serial: &mut SerialPort<'static, B>,
+    display: &mut DISPLAY,
+    index: &mut u32,
+    size: &mut u32,
+) {
+    while usb_dev.poll(&mut [serial]) {
+        let mut buff = [255u8; 32];
+        match serial.read(&mut buff) {
+            Ok(count) if count > 0 => match size {
+                0 => {
+                    let new_size = BigEndian::read_u32(&buff[0..4]);
+                    *size = new_size;
+                }
+                _ => {
+                    for (i, byte) in buff[0..count].iter().enumerate() {
+                        display.buff[*index as usize + i] = *byte;
+                    }
+                    *index += count as u32;
+                }
+            },
+            _ => {}
+        }
+    }
+    if *size > 0 && *index >= *size {
+        *size = 0;
+        *index = 0;
+        display.update().unwrap();
+    }
+}
 
 fn get_bit(x: u32, y: u32, width: u32, height: u32) -> (u32, u8) {
     (y / 8 + (height - 1 - x) * (width / 8), 0x80 >> (y % 8))
@@ -270,14 +339,14 @@ fn get_bit(x: u32, y: u32, width: u32, height: u32) -> (u32, u8) {
 impl<'a, SPI, CS, BUSY, DC, RESET> Drawing<Color> for Display<'a, SPI, CS, BUSY, DC, RESET>
 where
     SPI: embedded_hal::blocking::spi::Write<u8>,
-    CS: embedded_hal::digital::OutputPin,
-    BUSY: embedded_hal::digital::InputPin,
-    DC: embedded_hal::digital::OutputPin,
-    RESET: embedded_hal::digital::OutputPin,
+    CS: embedded_hal::digital::v2::OutputPin,
+    BUSY: embedded_hal::digital::v2::InputPin,
+    DC: embedded_hal::digital::v2::OutputPin,
+    RESET: embedded_hal::digital::v2::OutputPin,
 {
     fn draw<T>(&mut self, item_pixels: T)
     where
-        T: Iterator<Item = Pixel<Color>>,
+        T: IntoIterator<Item = Pixel<Color>>,
     {
         for Pixel(UnsignedCoord(x, y), colour) in item_pixels {
             self.set_pixel(x, y, colour);
